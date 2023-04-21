60cef36d979eb309f1cb5dcf64d2f7977a525804
Bruno <brpadilha.dev@gmail.com>
2023-04-21 20:57:17 -0300

    add origin cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: '*',
-    origin: '*',
+    origin: [
+      'http://localhost:5173',
+      'https://frontend-ammo-varejo-challenge.vercel.app',
+    ],
     credentials: true,
   });
   await app.listen(port);
